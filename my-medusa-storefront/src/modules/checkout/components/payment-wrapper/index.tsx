e759568a7fac61540709079a08e1085ded24b2b2
Felipe-heide <felipe.heide7@gmail.com>
2023-12-03 13:59:35 -0300

    Primer commit

---
+++ my-medusa-storefront/src/modules/checkout/components/payment-wrapper/index.tsx
@@ -0,0 +1,60 @@
+import { PaymentSession } from "@medusajs/medusa"
+import { Elements } from "@stripe/react-stripe-js"
+import { loadStripe, StripeElementsOptions } from "@stripe/stripe-js"
+import React from "react"
+
+type WrapperProps = {
+  paymentSession?: PaymentSession | null
+}
+
+const Wrapper: React.FC<WrapperProps> = ({ paymentSession, children }) => {
+  const isStripe = paymentSession?.provider_id?.includes("stripe")
+
+  if (isStripe && paymentSession) {
+    return (
+      <StripeWrapper paymentSession={paymentSession}>{children}</StripeWrapper>
+    )
+  }
+
+  return <div>{children}</div>
+}
+
+const stripeKey = 
+  process.env.NEXT_PUBLIC_STRIPE_KEY||
+  "pk_test_51MOWnmJYOorQDptO86Eq9FbMsnI5avarVgkz286uDE6J33vxKYkkUF9rgOCwcyGEAGVYkST8JyKWZaGGBvlFYoSq005FK5Yi00"
+const stripePromise = stripeKey ? loadStripe(stripeKey) : null
+
+const StripeWrapper: React.FC<WrapperProps> = ({
+  paymentSession,
+  children,
+}) => {
+  const options: StripeElementsOptions = {
+    clientSecret: paymentSession!.data?.client_secret as string | undefined,
+  }
+
+  if (!stripeKey) {
+    throw new Error(
+      "Stripe key is missing. Set NEXT_PUBLIC_STRIPE_KEY environment variable."
+    )
+  }
+
+  if (!stripePromise) {
+    throw new Error(
+      "Stripe promise is missing. Make sure you have provided a valid Stripe key."
+    )
+  }
+
+  if (!paymentSession?.data?.client_secret) {
+    throw new Error(
+      "Stripe client secret is missing. Cannot initialize Stripe."
+    )
+  }
+
+  return (
+    <Elements options={options} stripe={stripePromise}>
+      {children}
+    </Elements>
+  )
+}
+
+export default Wrapper
